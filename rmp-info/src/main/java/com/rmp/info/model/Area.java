907420a519b5ed70cfb9e89e6bf93661002115e0
linwen <linwen@yandui.com>
2018-10-09 17:46:59 +0800

    u

---
+++ rmp-info/src/main/java/com/rmp/info/model/Area.java
@@ -0,0 +1,39 @@
+package com.rmp.info.model;
+
+import com.rmp.info.base.model.Model;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 地区
+ * 
+ * t_area
+ *
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Area extends Model implements Serializable {
+    /**
+     * 
+     */
+    private Long id;
+
+    /**
+     * 
+     */
+    private Long pid;
+
+    /**
+     * 
+     */
+    private String name;
+
+    private static final long serialVersionUID = 1L;
+}

1dba93099591da92aa554e9fdb14c282eb18f2d2
linwen <linwen@yandui.com>
2018-11-23 18:08:10 +0800

    u

--- rmp-auto/src/main/java/com/rmp/auto/model/UserRemindBean.java
+++ rmp-auto/src/main/java/com/rmp/auto/model/UserRemindBean.java
@@ -3,20 +3,19 @@ package com.rmp.auto.model;
 import com.rmp.info.model.UserRemind;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@SuppressWarnings("serial")
+@Data
+@EqualsAndHashCode(callSuper=false)
+@NoArgsConstructor
 public class UserRemindBean extends UserRemind {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5401334442586157683L;
 	
 	@Builder
-	public UserRemindBean(Long id, Long userId, Integer type, Long typeId, Integer advanceDate, Integer advanceDay, Integer remindDate, Integer isDelete, Integer version, Long createTime, Long updateTime, Integer ymd, Integer md, Integer d, Integer w) {
+	public UserRemindBean(Long id, Long userId, Integer type, Long typeId, Integer advanceDate, Integer advanceDay, Integer remindDate, Integer isDelete, Integer version, Long createTime, Long updateTime
+			, Integer ymd, Integer md, Integer d, Integer w) {
 		super(id, userId, type, typeId, advanceDate, advanceDay, remindDate, isDelete, version, createTime, updateTime);
 		this.ymd = ymd;
 		this.md = md;

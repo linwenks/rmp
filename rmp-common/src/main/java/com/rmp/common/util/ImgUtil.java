49928ed60a84736c63adcc3d953adb9984d46ad2
linwen <linwen@yandui.com>
2018-12-10 17:26:21 +0800

    u

--- rmp-common/src/main/java/com/rmp/common/util/ImgUtil.java
+++ rmp-common/src/main/java/com/rmp/common/util/ImgUtil.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 import net.coobird.thumbnailator.Thumbnails;
 
 public class ImgUtil {
+	
+	public static final String SIZE_10x10 = "10x10";
+	public static final String SIZE_20x20 = "20x20";
+	public static final String SIZE_50x50 = "50x50";
+	
+	
 
 	public static void main(String[] args) throws IOException {
 		Thumbnails.of("images/test.jpg").size(200, 300).toFile("C:/image_200x300.jpg");
